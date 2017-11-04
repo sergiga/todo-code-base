46d411fbd06edd33de0e0ab9219eac5cd9d5b452
Sergio Garcia <srgarcia91@gmail.com>
2017-11-04 23:42:19 +0100

    List all todos present on component data

---
+++ src/components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <todo-form @addTodo="addTodo" />
+    <ul>
+      <li v-for="todo in todos"
+        :key="todo.id"
+        class="list-item">
+        {{ todo.description }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoForm from '@/components/TodoForm';
+
+let nextId = 0;
+
+export default {
+  name: 'todo-list',
+  components: {
+    TodoForm,
+  },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo(todo) {
+      const newTodo = Object.assign({}, todo, { id: nextId });
+      this.todos.push(newTodo);
+      nextId += 1;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
