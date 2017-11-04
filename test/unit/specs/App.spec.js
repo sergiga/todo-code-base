077bb26f2c18b17b2b4d3656f9f30cfc982d1e52
Sergio Garcia <srgarcia91@gmail.com>
2017-11-04 17:52:00 +0100

    App renders the TodoList component

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -1,12 +1,19 @@
 import App from '@/App';
+import TodoList from '@/components/TodoList';
 import { shallow } from 'vue-test-utils';
 
 describe('App.vue', () => {
   it('should display the app header', () => {
     const wrapper = shallow(App);
-    const { vm } = wrapper;
 
-    expect(vm.$el.querySelector('h1').textContent)
+    expect(wrapper.find('h1').text())
       .to.equal('¿Qué Hacer?');
+  });
+
+  it('renders the TodoList component', () => {
+    const wrapper = shallow(App);
+
+    expect(wrapper.contains(TodoList))
+      .to.be.true;
   });
 });
